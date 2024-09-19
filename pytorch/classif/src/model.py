3d80df51784191ba6961744111ad9fbbcaff634e
Chad Estoup--Streiff <43888084+ChadEstoupStreiff@users.noreply.github.com>
2024-09-19 10:19:17 +0200

    Add files via upload

--- pytorch/classif/src/model.py
+++ pytorch/classif/src/model.py
@@ -1,65 +1,8 @@
+from typing import Tuple, Union
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
-
-class ConvNet(nn.Module):
-    # !! Work only with 224x224 images !!
-    # Fast but not super smart
-    def __init__(self, nbr_classes):
-        super(ConvNet, self).__init__()
-
-        self.conv1_1 = nn.Conv2d(
-            3, 3, kernel_size=(3, 1), padding=(1, 0), stride=(2, 1)
-        )
-        self.conv1_2 = nn.Conv2d(
-            3, 16, kernel_size=(1, 3), padding=(0, 1), stride=(1, 2)
-        )
-
-        self.conv2_1 = nn.Conv2d(
-            16, 16, kernel_size=(3, 1), padding=(1, 0), stride=(2, 1)
-        )
-        self.conv2_2 = nn.Conv2d(
-            16, 32, kernel_size=(1, 3), padding=(0, 1), stride=(1, 2)
-        )
-
-        self.conv3_1 = nn.Conv2d(
-            32, 32, kernel_size=(3, 1), padding=(1, 0), stride=(2, 1)
-        )
-        self.conv3_2 = nn.Conv2d(
-            32, 64, kernel_size=(1, 3), padding=(0, 1), stride=(1, 2)
-        )
-
-        self.conv4_1 = nn.Conv2d(
-            64, 64, kernel_size=(3, 1), padding=(1, 0), stride=(2, 1)
-        )
-        self.conv4_2 = nn.Conv2d(
-            64, 128, kernel_size=(1, 3), padding=(0, 1), stride=(1, 2)
-        )
-
-        self.bn1 = nn.BatchNorm2d(16)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.bn4 = nn.BatchNorm2d(128)
-
-        self.dropout = nn.Dropout(0.2)
-
-        self.fc1 = nn.Linear(128 * 14 * 14, 1024)
-        self.fc2 = nn.Linear(1024, nbr_classes)
-
-
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1_2(self.conv1_1(x))))
-        x = F.relu(self.bn2(self.conv2_2(self.conv2_1(x))))
-        x = F.relu(self.bn3(self.conv3_2(self.conv3_1(x))))
-        x = F.relu(self.bn4(self.conv4_2(self.conv4_1(x))))
-
-        x = x.view(x.size(0), -1)
-
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-
-        return x
 
 
 class ResidualConvBlock(nn.Module):
@@ -75,81 +18,112 @@ class ResidualConvBlock(nn.Module):
 
     def forward(self, x):
         # Double conv layer
-        out = self.conv1_2(self.conv1_1(F.relu(self.bn1(x))))
-        out = self.conv2_2(self.conv2_1(F.relu(self.bn2(out))))
+        out = self.conv1_2(self.conv1_1(F.leaky_relu(self.bn1(x))))
+        out = self.conv2_2(self.conv2_1(F.leaky_relu(self.bn2(out))))
 
         return out + x
 
 
 class ResidualUpLayer(nn.Module):
+    # Keep the residuality even when downsizing, upgrade compare to a classic ResNet
     def __init__(self, input_size: int) -> None:
         super().__init__()
-        self.conv = nn.Conv2d(input_size, input_size*2, padding=1, kernel_size=3, stride=2)
+        self.conv = nn.Conv2d(
+            input_size, input_size * 2, padding=1, kernel_size=3, stride=2
+        )
         self.maxPool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.avgPool = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
-    
+
     def forward(self, x):
-        return torch.cat((self.maxPool(x), self.avgPool(x)), dim=1) + self.conv(x)
+        residual = self.conv(x)
+        x = torch.cat((self.maxPool(x), self.avgPool(x)), dim=1)
+        return x + residual
 
 
 class ResNet(nn.Module):
-    # !! Work only with 224x224 images !!
-
-    def __init__(self, nbr_classes: int) -> None:
+    def __init__(self, input_size: Union[int, Tuple[int]], nbr_classes: int) -> None:
         super(ResNet, self).__init__()
+        if isinstance(input_size, int):
+            input_size = (input_size, input_size)
 
         # Divide images
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=2)
+        self.conv0 = nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=2)
         self.maxPool = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        # layers
         self.layer1_1 = ResidualConvBlock(64)
         self.layer1_2 = ResidualConvBlock(64)
+        # self.layer1_3 = ResidualConvBlock(64)
 
         self.conv2 = ResidualUpLayer(64)
         self.layer2_1 = ResidualConvBlock(128)
         self.layer2_2 = ResidualConvBlock(128)
+        # self.layer2_3 = ResidualConvBlock(128)
+        # self.layer2_4 = ResidualConvBlock(128)
 
         self.conv3 = ResidualUpLayer(128)
         self.layer3_1 = ResidualConvBlock(256)
         self.layer3_2 = ResidualConvBlock(256)
         self.layer3_3 = ResidualConvBlock(256)
+        # self.layer3_4 = ResidualConvBlock(256)
+        # self.layer3_5 = ResidualConvBlock(256)
+        # self.layer3_6 = ResidualConvBlock(256)
+        # self.layer3_7 = ResidualConvBlock(256)
 
         self.conv4 = ResidualUpLayer(256)
         self.layer4_1 = ResidualConvBlock(512)
         self.layer4_2 = ResidualConvBlock(512)
+        # self.layer4_3 = ResidualConvBlock(512)
+        # self.layer4_4 = ResidualConvBlock(512)
+        # self.layer4_5 = ResidualConvBlock(512)
 
         # 2 Linear ouput layers
-        self.fc1 = nn.Linear(512 * 7 * 7, 1024)
-        self.fc2 = nn.Linear(1024, nbr_classes)
-
-        self.dropout = nn.Dropout(0.2)
+        print((input_size[0] // 32 + (0 if input_size[0] % 32 == 0 else 1)))
+        print((input_size[1] // 32 + (0 if input_size[1] % 32 == 0 else 1)))
+        self.fc1 = nn.Linear(
+            512
+            * (input_size[0] // 32 + (0 if input_size[0] % 32 == 0 else 1))
+            * (input_size[1] // 32 + (0 if input_size[1] % 32 == 0 else 1)),
+            1024,
+        )
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, nbr_classes)
 
     def forward(self, x):
         # ResNet blocks
-        x = self.conv1(x)
+        x = self.conv0(x)
         x = self.maxPool(x)
+
         x = self.layer1_1(x)
         x = self.layer1_2(x)
+        # x = self.layer1_3(x)
 
         x = self.conv2(x)
         x = self.layer2_1(x)
         x = self.layer2_2(x)
+        # x = self.layer2_3(x)
+        # x = self.layer2_4(x)
 
         x = self.conv3(x)
         x = self.layer3_1(x)
         x = self.layer3_2(x)
         x = self.layer3_3(x)
+        # x = self.layer3_4(x)
+        # x = self.layer3_5(x)
+        # x = self.layer3_6(x)
+        # x = self.layer3_7(x)
 
         x = self.conv4(x)
         x = self.layer4_1(x)
         x = self.layer4_2(x)
+        # x = self.layer4_3(x)
+        # x = self.layer4_4(x)
+        # x = self.layer4_5(x)
 
         # Flatten dynamicly
         x = x.view(x.size(0), -1)
 
         # Output network
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
+        x = F.dropout(F.leaky_relu(self.fc1(x)), 0.4)
+        x = F.dropout(F.leaky_relu(self.fc2(x)), 0.4)
+        x = self.fc3(x)
         return x
--- pytorch/classif/src/train.py
+++ pytorch/classif/src/train.py
@@ -1,57 +1,76 @@
 import os
+import sys
 import time
 
-import torchvision
-from eval import do_evaluation
-from model import ConvNet, ResNet
-from torchvision import datasets, transforms
-from gpubalancer import GPUBalancerClient
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+import torchvision
+from eval import do_evaluation
+from model import ResNet
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
 from torchsummary import summary
-import logging
+from torchvision import datasets, transforms
+from tqdm import tqdm
 
 # Device configuration
-logging.basicConfig(level=logging.INFO)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ################ Hyper-parameters
-SAVE_PATH = "../models"
-DATA_PATH = "../data/birds"
-MODEL_NAME = "birds3"
+SAVE_PATH = "./models/"
+DATA_PATH = "./resnet_data/"
+MODEL_NAME = "resnet_v3"
+BINARY_MODEL_POSITIVE_INDEX = 0
 
-MODEL_INPUT_SIZE = 224
+START_EPOCH = 1
+MODEL_CHECKPOINT = f"models/resnet_v2_{START_EPOCH}.pth"  # None to start from scratch model
+MODEL_CHECKPOINT = None  # None to start from scratch model
+
+MODEL_INPUT_SIZE = (128, 256)
+# MODEL_INPUT_SIZE = (128, 646)
 
 NUM_EPOCHS = 3000
-BATCH_SIZE = 256
+BATCH_SIZE = 512
 LEARNING_RATE = 0.00005
 
 VERBOSE_PERIOD = 1
 EVAL_PERIOD = 1
+# None for input in shell, path for a file
+# LOG_FILE = None
+LOG_FILE = "train.log"  
 
-################ Sport data loading
-writer = SummaryWriter(f"../runs/{MODEL_NAME}")
-logging.info("Loading dataset...")
+if LOG_FILE is not None:
+    log_file = open(LOG_FILE, "a")
+    sys.stdout = log_file
+    sys.stderr = log_file
+
+writer = SummaryWriter(f"./runs/{MODEL_NAME}")
+
+
+################ data loading
+
+print("Loading dataset...")
 training_transform = transforms.Compose(
     [
-        transforms.Resize((MODEL_INPUT_SIZE, MODEL_INPUT_SIZE)),
-        transforms.RandomResizedCrop(size=224, scale=(0.8, 1.0)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomRotation(10),
-        transforms.RandomPerspective(distortion_scale=0.2, p=0.5),
+        # transforms.RandomResizedCrop(size=224, scale=(0.8, 1.0)),
+        transforms.Resize((MODEL_INPUT_SIZE[0], MODEL_INPUT_SIZE[1])),
+        # transforms.ColorJitter(
+        #     brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
+        # ),  # Optional for spectrogram
+        # # transforms.RandomRotation(10),  # Small rotations for invariance
+        # transforms.RandomPerspective(
+        #     distortion_scale=0.2, p=0.5
+        # ),  # Simulates perspective changestransforms.ToTensor(),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        ),  # Normalize with ImageNet standards
+        ),  # Normalize with ImageNet stats
     ]
 )
 transform = transforms.Compose(
     [
-        transforms.Resize((MODEL_INPUT_SIZE, MODEL_INPUT_SIZE)),
+        transforms.Resize((MODEL_INPUT_SIZE[0], MODEL_INPUT_SIZE[1])),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
@@ -62,105 +81,121 @@ transform = transforms.Compose(
 train_dataset = datasets.ImageFolder(
     root=os.path.join(DATA_PATH, "train"), transform=training_transform
 )
+# To counter imbalance dataset
+class_counts = [
+    len(np.where(np.array(train_dataset.targets) == t)[0])
+    for t in np.unique(train_dataset.targets)
+]
+class_weights = 1.0 / torch.tensor(class_counts, dtype=torch.float)
+print(class_weights)
+sample_weights = class_weights[train_dataset.targets]
+print(sample_weights)
+sampler = WeightedRandomSampler(
+    weights=sample_weights, num_samples=len(sample_weights), replacement=True
+)
+
 eval_dataset = datasets.ImageFolder(
     root=os.path.join(DATA_PATH, "eval"), transform=transform
 )
-test_dataset = datasets.ImageFolder(
-    root=os.path.join(DATA_PATH, "test"), transform=transform
+
+print(f"Train dataset: Number of classes: {len(train_dataset.classes)}")
+print(f"Train dataset: Class to index mapping: \n{train_dataset.class_to_idx}")
+print(f"Eval dataset: Number of classes: {len(train_dataset.classes)}")
+print(f"Eval dataset: Class to index mapping: \n{train_dataset.class_to_idx}")
+
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, num_workers=4, sampler=sampler
 )
-logging.info(f"Train dataset: Number of classes: {len(train_dataset.classes)}")
-logging.info(f"Train dataset: Class to index mapping: \n{train_dataset.class_to_idx}")
-logging.info(f"Eval dataset: Number of classes: {len(train_dataset.classes)}")
-logging.info(f"Eval dataset: Class to index mapping: \n{train_dataset.class_to_idx}")
-logging.info(f"Test dataset: Number of classes: {len(test_dataset.classes)}")
-logging.info(f"Test dataset: Class to index mapping: \n{test_dataset.class_to_idx}")
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
-
-examples = iter(test_loader)
+examples = iter(train_loader)
 example_data, example_targets = next(examples)
 classes = train_dataset.classes
 
 img_grid = torchvision.utils.make_grid(example_data)
-writer.add_image("mnist_images", img_grid)
+writer.add_image("images", img_grid)
 
 ################ Custom Model
-with GPUBalancerClient(importance=-1,
-    max_gpu_load=11000,
-    title="PyTorch test",
-    description="C'est Chad qui fou la merde :)",
-    host="10.10.41.2", port=8043):
+print(f"Creating model {MODEL_NAME} ...")
+model = ResNet(MODEL_INPUT_SIZE, len(train_dataset.classes)).to(device)
+if MODEL_CHECKPOINT is not None:
+    model.load_state_dict(torch.load(MODEL_CHECKPOINT, weights_only=False))
+    print(f"Checkpoint loaded: {MODEL_CHECKPOINT}")
+model.eval()
+summary(model, (3, MODEL_INPUT_SIZE[0], MODEL_INPUT_SIZE[1]))
 
-    logging.info("Creating model...")
-    model = ResNet(len(train_dataset.classes)).to(device)
-    summary(model, (3, MODEL_INPUT_SIZE, MODEL_INPUT_SIZE))
+################ Loss & Optimizer
 
-    ################ Loss & Optimizer
+lossfct = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2)
+writer.add_graph(model, example_data.to(device))
+writer.add_scalar("learning_rate", scheduler.get_last_lr()[0], 0)
 
-    lossfct = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2)
-    writer.add_graph(model, example_data.to(device))
-    writer.add_scalar("learning_rate", scheduler.get_last_lr()[0], 0)
+################ Training
+print("Started training...")
+model.train()
+for epoch in range(START_EPOCH, NUM_EPOCHS):
+    start_time = time.time()
+    running_loss = 0.0
+    running_correct = 0
+    total_samples = 0
+    for i, (images, labels) in tqdm(
+        enumerate(train_loader),
+        total=len(train_loader),
+        desc=f"Epoch {epoch+1:3}/{NUM_EPOCHS}",
+        mininterval=60,
+    ):
+        images = images.to(device)
+        labels = labels.to(device)
 
-    ################ Training
-    logging.info("Started training...")
-    for epoch in range(NUM_EPOCHS):
+        # Forward pass
+        outputs = model(images)
+        loss = lossfct(outputs, labels)
 
-        start_time = time.time()
-        running_loss = 0.0
-        running_correct = 0
-        total_samples = 0
-        for i, (images, labels) in tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1:3}/{NUM_EPOCHS}"):
-            images = images.to(device)
-            labels = labels.to(device)
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            # Forward pass
-            outputs = model(images)
-            loss = lossfct(outputs, labels)
+        running_loss += loss.item() * images.size(0)
+        _, predicted = torch.max(outputs.data, 1)
+        running_correct += (predicted == labels).sum().item()
+        total_samples += labels.size(0)
+    scheduler.step()
+    writer.add_scalar("learning_rate", scheduler.get_last_lr()[0], epoch + 1)
 
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    # Compute average loss and accuracy for the epoch
+    epoch_loss = running_loss / total_samples
+    epoch_accuracy = running_correct / total_samples
 
-            running_loss += loss.item() * images.size(0)
-            _, predicted = torch.max(outputs.data, 1)
-            running_correct += (predicted == labels).sum().item()
-            total_samples += labels.size(0)
-        scheduler.step()
-        writer.add_scalar("learning_rate", scheduler.get_last_lr()[0], epoch+1)
+    if epoch % VERBOSE_PERIOD == 0:
+        print(
+            f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f} in {(time.time() - start_time):.3f}s"
+        )
 
-        # Compute average loss and accuracy for the epoch
-        epoch_loss = running_loss / total_samples
-        epoch_accuracy = running_correct / total_samples
+    # Log metrics
+    writer.add_scalar("train_loss", epoch_loss, epoch)
+    writer.add_scalar("train_accuracy", epoch_accuracy, epoch)
 
-        if epoch % VERBOSE_PERIOD == 0:
-            logging.info(
-                f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f} in {(time.time() - start_time):.3f}s"
-            )
+    if epoch % EVAL_PERIOD == 0:
+        print("Evaluating model...")
 
-        # Log metrics
-        writer.add_scalar("train_loss", epoch_loss, epoch)
-        writer.add_scalar("train_accuracy", epoch_accuracy, epoch)
+        if not os.path.exists(SAVE_PATH):
+            os.makedirs(SAVE_PATH)
+        PATH = os.path.join(SAVE_PATH, f"{MODEL_NAME}-{epoch+1}.pth")
+        torch.save(model.state_dict(), PATH)
+        model.eval()
+        metrics = do_evaluation(model, eval_dataset, binary_prblm_positive_index=BINARY_MODEL_POSITIVE_INDEX)
+        model.train()
+        writer.add_scalar("eval_accuracy", metrics["accuracy"], epoch)
+        writer.add_scalar("eval_weighted_accuracy", metrics["weighted_accuracy"], epoch)
+        writer.add_scalar("eval_precision", metrics["precision"], epoch)
+        writer.add_scalar("eval_recall", metrics["recall"], epoch)
+        writer.add_scalar("eval_f1_score", metrics["f1_score"], epoch)
+        writer.add_scalar("eval_weighted_f1_score", metrics["weighted_f1_score"], epoch)
 
-        if epoch % EVAL_PERIOD == 0:
-            logging.info("Evaluating model...")
-
-            if not os.path.exists(SAVE_PATH):
-                os.makedirs(SAVE_PATH)
-            PATH = os.path.join(SAVE_PATH, f"{MODEL_NAME}-{epoch+1}.pth")
-            torch.save(model.state_dict(), PATH)
-            model.eval()
-            eval_accuracy = do_evaluation(
-                model, eval_dataset
-            )
-            model.train()
-            writer.add_scalar("eval_accuracy", eval_accuracy, epoch)
-
-    logging.info("Finished Training. Saving model...")
-    if not os.path.exists(SAVE_PATH):
-        os.makedirs(SAVE_PATH)
-    PATH = os.path.join(SAVE_PATH, f"{MODEL_NAME}-final.pth")
-    torch.save(model.state_dict(), PATH)
+print("Finished Training. Saving model...")
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+PATH = os.path.join(SAVE_PATH, f"{MODEL_NAME}-final.pth")
+torch.save(model.state_dict(), PATH)
